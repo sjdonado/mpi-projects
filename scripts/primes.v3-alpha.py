252eced557253b6df725843685a0543ea27172a8
Juan Rodriguez <jsrd98@gmail.com>
2019-04-02 11:39:35 -0500

    chore: v3-alpha behavior

--- scripts/primes.v3-alpha.py
+++ scripts/primes.v3-alpha.py
@@ -28,14 +28,16 @@ def main():
   for num in xrange(rank * size + min, max, size * size):
     if max - unread_indexes == num: container_range = range(0, unread_indexes)
     # print 'rank', rank, 'num', num, 'max - size + 1 - unread_indexes', max - unread_indexes, 'container_range', container_range
-    for cont in container_range:
+    for container in container_range:
       i = 2
-      x = num + cont
-      while i <= x / i:
-        if x % i == 0:
+      print 'rank', rank, 'num', (num + container)
+      while i <= (num + container) / i:
+        if (num + container) % i == 0:
           non_primes_cont += 1
           break
         i += 1 if i == 2 else 2  
+
+  print 'FINISHED ---->', 'rank', rank
   # for num in xrange(rank * size + min, max, size * size):
   #   # if max - unread_indexes == num: container_range = range(0, unread_indexes)
   #   # print 'rank', rank, 'num', num, 'max - size + 1 - unread_indexes', max - unread_indexes, 'container_range', container_range
@@ -47,9 +49,14 @@ def main():
   #       primes_cont = is_prime(primes_cont, max - (num + container) + min);
   #     dir = not dir
 
+  # primes = numpy.array(non_primes_cont, 'i')
+  # result = numpy.array(0, 'i')
+
   result = comm.reduce(sendobj=non_primes_cont, root=root_process, op=MPI.SUM)
+  # comm.Reduce([primes, MPI.INT], [result, MPI.INT], op=MPI.SUM, root=root_process)
 
   if rank == root_process:
+    print 'result', result, 'max', max, 'min', min, (max - min) - result
     print 'El numero de primos de', digits, 'digitos es', (max - min) - result, 'Tiempo:', MPI.Wtime() - start_time
   # comm.Disconnect()
 
